f8b6dc0b30ef0266ebd1587a9523a66ecbf471a7
ciuvercaionut <ciuvercaionut@gmail.com>
2020-02-27 14:40:13 +0200

    second test to AddToCompare

--- src/main/java/com/firestarters/page/ProductListPage.java
+++ src/main/java/com/firestarters/page/ProductListPage.java
@@ -2,10 +2,13 @@ package com.firestarters.page;
 
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
 import java.util.Random;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ProductListPage extends AbstractPage {
@@ -13,11 +16,14 @@ public class ProductListPage extends AbstractPage {
     @FindBy(css = ".product-image[title*= 'Core']")
     private WebElementFacade specificProduct;
 
-    @FindBy(css = ".link-compare")
-    private List<WebElementFacade> addToCompareLinksList;
+    @FindBy(css = ".products-grid .item.last")
+    private List<WebElementFacade> porductsList;
 
     @FindBy(css = ".success-msg")
     private WebElementFacade successMessage;
+
+    @FindBy(css = ".block-compare")
+    private WebElementFacade compareProductsMenu;
 
     //Ciuverca Ionut
     public void selectAProduct(){
@@ -25,10 +31,26 @@ public class ProductListPage extends AbstractPage {
     }
 
     //Ciuverca Ionut
-    public void clickOnAddToCompare(){
+    public WebElementFacade getRandomProduct(){
         Random random = new Random();
-        WebElementFacade randomAdToCartLink = addToCompareLinksList.get(random.nextInt(addToCompareLinksList.size()));
-        randomAdToCartLink.click();
-        assertTrue(successMessage.getText().contains("added to comparison list"));
+        WebElementFacade randomProduct = porductsList.get(random.nextInt(porductsList.size()));
+        return randomProduct;
+    }
+
+    //Ciuverca Ionut
+    public void clickOnAddToCompare(){
+        WebElementFacade randomProduct= getRandomProduct();
+        String productTitle = randomProduct.findElement(By.cssSelector(".product-name")).getText().toLowerCase();
+        randomProduct.findElement(By.cssSelector(".link-compare")).click();
+        String message = successMessage.getText().toLowerCase();
+        assertTrue(message.contains(productTitle));
+    }
+
+    //Ciuverca Ionut
+    public void addToCompare(){
+        WebElementFacade randomProduct = getRandomProduct();
+        randomProduct.findElement(By.cssSelector(".link-compare")).click();
+        assertTrue(compareProductsMenu.isDisplayed());
+
     }
 }
