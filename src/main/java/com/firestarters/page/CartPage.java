4c3bffbc368b2a4f04bc598c0d491b131ad97e9f
agotanemes <nem_agi_96@yahoo.com>
2020-02-27 11:23:28 +0200

    Login test for unregistered user Shoping cart products

--- src/main/java/com/firestarters/page/CartPage.java
+++ src/main/java/com/firestarters/page/CartPage.java
@@ -2,6 +2,7 @@ package com.firestarters.page;
 
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.WebElementFacade;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
@@ -15,6 +16,20 @@ public class CartPage extends  AbstractPage{
 
     @FindBy(css = "#shopping-cart-table > tbody")
     private WebElementFacade listOfProductsInCart;
+
+    //Agota
+    @FindBy(css="#shopping-cart-table>tbody>tr>td[class='product-cart-price']")
+    private List<WebElementFacade> listOfProductPricesInCart;
+    @FindBy(css ="#shopping-cart-table>tbody>tr>td[class='product-cart-actions']>input")
+    private List<WebElementFacade> listOfProductQtysInCart;
+    @FindBy(css="#shopping-cart-table>tbody>tr>td[class='product-cart-actions']>button")
+    private List<WebElementFacade> listOfProductUpdateButtonsToChangeQty;
+    @FindBy(css=".product-cart-total>span span[class='price']")
+    private List<WebElementFacade> listOfSubtotalsInCart;
+    @FindBy(css = ".a-right>strong>span[class='price']")
+    private WebElement totalPrice;
+
+    //-------------------
 
 
 
